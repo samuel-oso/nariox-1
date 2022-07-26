d2ae897587d094caff586fcd54f32939181c0176
unknown <ososamuel035@gmail.com>
2022-07-26 13:23:36 +0100

    Analytics overview collapse

--- src/components/analyticsOverview/AnalyticsOverview.tsx
+++ src/components/analyticsOverview/AnalyticsOverview.tsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import "../../styles/components/AnalyticsOverview.css";
 import { FaEllipsisV } from "react-icons/fa";
 import { Tabs } from "@mantine/core";
 import SubscribersChart from "./SubscribersChart";
 import TotalViewsChart from "./TotalViewsChart";
 import BounceRateChart from "./BounceRateChart";
+import { FiUserPlus } from "react-icons/fi";
+import { TbLogout } from "react-icons/tb";
+import { TbRefreshDot } from "react-icons/tb";
+
+import { Collapse, Divider } from "@mantine/core";
 
 const AnalyticsOverview = () => {
+  const [overview, setOverview] = useState(false);
+
   return (
     <div className="analyticsOverview-Card">
       <div className="flex items-center justify-between mb-2">
         <h5 className="text-base font-semibold mb-0 ">OVERVIEWS</h5>
-        <FaEllipsisV className="cursor-pointer" />
+        <FaEllipsisV
+          className="cursor-pointer"
+          onClick={() => setOverview((o) => !o)}
+        />
       </div>
 
       <Tabs className="analytics-Tab">
@@ -45,6 +56,24 @@ const AnalyticsOverview = () => {
           <BounceRateChart />
         </Tabs.Tab>
       </Tabs>
+
+      <Collapse in={overview} className="ellipseCollapse">
+        <div className="py-1">
+          <div className="ellipseItem">
+            <TbRefreshDot />
+            <p>Refresh</p>
+          </div>
+          <div className="ellipseItem">
+            <FiUserPlus />
+            <p>Add New</p>
+          </div>
+          <Divider className="my-3" />
+          <div style={{ color: "var(--danger)" }} className="ellipseItem">
+            <TbLogout />
+            <p>Exit</p>
+          </div>
+        </div>
+      </Collapse>
     </div>
   );
 };
