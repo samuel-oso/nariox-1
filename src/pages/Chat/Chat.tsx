c379c3fb788245900d7f6d4dcb27b732f597d079
unknown <ososamuel035@gmail.com>
2022-08-08 23:40:19 +0100

    styled the dropdown and the profile picture

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -3,6 +3,7 @@ import ChatUsers from "./ChatUsers";
 import ChatArea from "./ChatArea";
 import { USERS, ChatUserType } from "./data";
 import { Grid } from "@mantine/core";
+import Layout from "../../components/Layout";
 
 // ChatApp
 const Chat = () => {
@@ -14,16 +15,18 @@ const Chat = () => {
   };
 
   return (
-    <>
-      <Grid>
-        <Grid.Col lg={5} xl={3}>
-          <ChatUsers onUserSelect={onUserChange} />
-        </Grid.Col>
-        <Grid.Col lg={7} xl={9}>
-          <ChatArea selectedUser={selectedUser} />
-        </Grid.Col>
-      </Grid>
-    </>
+    <Layout>
+      <div className="main-container">
+        <Grid>
+          <Grid.Col lg={5} xl={3}>
+            <ChatUsers onUserSelect={onUserChange} />
+          </Grid.Col>
+          <Grid.Col lg={7} xl={9}>
+            <ChatArea selectedUser={selectedUser} />
+          </Grid.Col>
+        </Grid>
+      </div>
+    </Layout>
   );
 };
 
--- src/pages/Chat/ChatUsers.tsx
+++ src/pages/Chat/ChatUsers.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Card, Dropdown } from "react-bootstrap";
+import { Paper, Collapse } from "@mantine/core";
+import { useClickOutside } from "@mantine/hooks";
+import "../../styles/pages/Chat.css";
 import classNames from "classnames";
 import { USERS, ChatUserType } from "./data";
-
-// images
 import profilePic from "../../images/avatar-2.jpg";
+import { TbDotsVertical } from "react-icons/tb";
+import { BsPeople } from "react-icons/bs";
+import { RiContactsFill } from "react-icons/ri";
+import { BsBookmark } from "react-icons/bs";
+import { BsFillPersonPlusFill } from "react-icons/bs";
+import { TbHelp } from "react-icons/tb";
+import { FiSettings } from "react-icons/fi";
+import { Input } from "@mantine/core";
+import { TbSearch } from "react-icons/tb";
 
 interface ChatUsersProps {
   onUserSelect: (value: ChatUserType) => void;
@@ -15,12 +24,13 @@ interface ChatUsersProps {
 const ChatUsers = ({ onUserSelect }: ChatUsersProps) => {
   const [user, setUser] = useState<ChatUserType[]>([...USERS]);
   const [selectedUser, setSelectedUser] = useState<ChatUserType>(USERS[1]);
-
   const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
 
-  /*
-   * toggle apps-dropdown
-   */
+  // close on click outside
+  const ref = useClickOutside(() => {
+    setDropdownOpen(!dropdownOpen);
+  });
+
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
@@ -52,149 +62,136 @@ const ChatUsers = ({ onUserSelect }: ChatUsersProps) => {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <div className="d-flex pb-2 border-bottom align-items-center">
+      <div className="chatUsers-Card">
+        <div className="flex items-center justify-between">
+          <div className="chatUsers-Profile">
             <img
               src={profilePic}
-              className="me-2 rounded-circle"
+              className="rounded-circle"
               height="48"
-              alt=""
+              alt="ChatProfilePic"
             />
-            <div>
-              <h5 className="my-0 fs-14">Shreyu N</h5>
-            </div>
-
-            <div className="flex-grow-1">
-              <ul className="list-inline text-end mb-0">
-                <Dropdown
-                  show={dropdownOpen}
-                  as="li"
-                  onToggle={toggleDropdown}
-                  className="list-inline-item fs-18"
-                >
-                  <Dropdown.Toggle
-                    id="dropdown-apps"
-                    as="a"
-                    onClick={toggleDropdown}
-                    className={classNames("text-dark", "cursor-pointer", {
-                      show: dropdownOpen,
-                    })}
-                  >
-                    <i className="bi bi-three-dots-vertical"></i>
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu className="dropdown-menu-end">
-                    <Dropdown.Item href="#/">
-                      <i className="bi bi-people fs-16 me-2"></i>New Group
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/">
-                      <i className="bi bi-person-lines-fill fs-16 me-2"></i>
-                      Contacts
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/">
-                      <i className="bi bi-bookmark fs-16 me-2"></i>Saved Message
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/">
-                      <i className="bi bi-person-plus fs-16 me-2"></i>Invite
-                      Friends
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/">
-                      <i className="bi bi-question-circle fs-16 me-2"></i>Help
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/">
-                      <i className="bi bi-gear fs-16 me-2"></i>Setting
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </ul>
-            </div>
+            <h5 className="">Micheal J</h5>
           </div>
-
           <div>
-            <form className="chat-search">
-              <div className="chat-search-box">
-                <div className="input-group">
-                  <button className="btn input-group-text" type="submit">
-                    <i className="uil uil-search"></i>
-                  </button>
-                  <input
-                    type="search"
-                    className="form-control"
-                    placeholder="Search..."
-                    id="top-search"
-                    onKeyUp={(e: any) => search(e.target.value)}
-                  />
+            <TbDotsVertical
+              onClick={toggleDropdown}
+              className={classNames("cursor-pointer text-lg", {
+                show: dropdownOpen,
+              })}
+            />
+          </div>
+        </div>
+
+        <div>
+          <form className="chat-search">
+            <div className="chat-search-box">
+              <div className="input-group">
+                <Input
+                  type="search"
+                  className="form-control"
+                  placeholder="Search..."
+                  icon={<TbSearch />}
+                  onKeyUp={(e: any) => search(e.target.value)}
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div className="pe-2">
+          {(user || []).map((user, index) => {
+            return (
+              <Link
+                to="#"
+                key={index}
+                className="text-body"
+                onClick={(e: any) => {
+                  activateUser(user);
+                }}
+              >
+                <div
+                  className={classNames("flex", "items-start", "p-2", {
+                    "bg-light": user.id === selectedUser.id,
+                  })}
+                >
+                  <div className="relative">
+                    <span
+                      className={classNames("user-status", {
+                        "bg-success": user.userStatus === "online",
+                        "bg-danger": user.userStatus === "busy",
+                        "bg-warning": user.userStatus === "away",
+                      })}
+                    ></span>
+                    <img
+                      src={user.avatar}
+                      className="rounded-circle"
+                      height="48"
+                      alt=""
+                    />
+                  </div>
+                  <div className="w-full overflow-hidden">
+                    <h5 className="mt-0 mb-0">
+                      <span className="float-right text-xs">
+                        {user.lastMessageOn}
+                      </span>
+                      {user.name}
+                    </h5>
+                    <p className="mt-1 mb-0">
+                      {user.totalUnread !== 0 && (
+                        <span className="float-right bg-danger text-white">
+                          {user.totalUnread}
+                        </span>
+                      )}
+                      <span
+                        className={classNames("w-72", {
+                          "text-success": user.totalUnread,
+                        })}
+                      >
+                        {user.lastMessage}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+
+        {/* Collapse */}
+        {dropdownOpen && (
+          <Paper ref={ref}>
+            <Collapse in={dropdownOpen} className="chatProfile-Collapse">
+              <div>
+                <div className="chatProfile-Item">
+                  <BsPeople />
+                  <p>New Group</p>
+                </div>
+                <div className="chatProfile-Item">
+                  <RiContactsFill />
+                  <p>Contacts</p>
+                </div>
+                <div className="chatProfile-Item">
+                  <BsBookmark />
+                  <p>Saved Message</p>
+                </div>
+                <div className="chatProfile-Item">
+                  <BsFillPersonPlusFill />
+                  <p>Invite Friends</p>
+                </div>
+                <div className="chatProfile-Item">
+                  <TbHelp />
+                  <p>Help</p>
+                </div>
+                <div className="chatProfile-Item">
+                  <FiSettings />
+                  <p>Setting</p>
                 </div>
               </div>
-            </form>
-          </div>
-
-          <div className="pe-2">
-            {(user || []).map((user, index) => {
-              return (
-                <Link
-                  to="#"
-                  key={index}
-                  className="text-body"
-                  onClick={(e: any) => {
-                    activateUser(user);
-                  }}
-                >
-                  <div
-                    className={classNames(
-                      "d-flex",
-                      "align-items-start",
-                      "p-2",
-                      {
-                        "bg-light": user.id === selectedUser.id,
-                      }
-                    )}
-                  >
-                    <div className="position-relative">
-                      <span
-                        className={classNames("user-status", {
-                          "bg-success": user.userStatus === "online",
-                          "bg-danger": user.userStatus === "busy",
-                          "bg-warning": user.userStatus === "away",
-                        })}
-                      ></span>
-                      <img
-                        src={user.avatar}
-                        className="me-2 rounded-circle"
-                        height="48"
-                        alt=""
-                      />
-                    </div>
-
-                    <div className="w-100 overflow-hidden">
-                      <h5 className="mt-0 mb-0 fs-14">
-                        <span className="float-end text-muted fs-12">
-                          {user.lastMessageOn}
-                        </span>
-                        {user.name}
-                      </h5>
-                      <p className="mt-1 mb-0 text-muted fs-14">
-                        {user.totalUnread !== 0 && (
-                          <span className="float-end badge bg-danger text-white">
-                            {user.totalUnread}
-                          </span>
-                        )}
-                        <span
-                          className={classNames("w-75", {
-                            "text-dark": user.totalUnread,
-                          })}
-                        >
-                          {user.lastMessage}
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
-        </Card.Body>
-      </Card>
+            </Collapse>
+          </Paper>
+        )}
+      </div>
     </>
   );
 };
